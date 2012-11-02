1f8590044724c93d5c67c08da2827a86e62b6564
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-11-02 18:42:36 -0400

    some cleanup

--- chatham/hooks/queue.py
+++ chatham/hooks/queue.py
@@ -1,13 +1,12 @@
 
 from fishhook import Hook
-from monomoy.core import db
 from chatham.queue import ChathamQueue
+
 
 class PackageAccepted(Hook):
     def fire(self, caller, event, obj):
         user = obj['user']['_id']
         package = obj['package']
-        changes = db.packages.find_one({"_id": package})
         q = ChathamQueue()
         job = q.enqueue(package, user)
         print "Job %s created." % (job)
--- chatham/queue.py
+++ chatham/queue.py
@@ -3,6 +3,7 @@ import datetime as dt
 from monomoy.core import db
 from fishhook import Hook
 
+
 class ChathamQueue(Hook):
     def __init__(self):
         self._db = db
