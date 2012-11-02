a656a89166eccc55b1ed67c5d7acc0440132ced0
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-11-02 18:29:05 -0400

    Adding in s'more goodies.

--- chatham/monomoy/hooks.py
+++ chatham/monomoy/hooks.py
@@ -1,6 +1,11 @@
 
 from fishhook import Hook
+from monomoy.core import db
+from chatham.queue import ChathamQueue
 
 class PackageAccepted(Hook):
     def fire(self, caller, event, obj):
-        pass
+        user = obj['user']
+        package = obj['package']
+        changes = db.packages.find_one({"_id": package})
+        build_for = changes['changes']['Distribution']
